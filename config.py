d98b4badf3b05dc6f24b22bd120a42124efd7380
Gabriel Teixeira <louistx@live.com>
2021-01-20 17:32:14 -0300

    alteracao no db para heroku

--- config.py
+++ config.py
@@ -1,24 +1,13 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-POSTGRES_URL = 'localhost:5432'
-POSTGRES_USER = 'postgres'
-POSTGRES_PASSWORD = 'postgres'
-POSTGRES_DB = 'assignme_dev'
 
 class Config:
     # BASE CONFIG
-    SECRET_KEY = '%yaoroe$abifz4%58yd1kxea6j&2y13i!3dby0wo)i0nmwxyne'
-    STATIC_FOLDER = 'static'
-    TEMPLATES_FOLDER = 'templates'
     # SQLAlchemy
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     uri_template = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'
-    SQLALCHEMY_DATABASE_URI = uri_template.format(
-        user=POSTGRES_USER,
-        pw=POSTGRES_PASSWORD,
-        url=POSTGRES_URL,
-        db=POSTGRES_DB)
+    SQLALCHEMY_DATABASE_URI = 'postgres://mvtmakidovwrsg:4c3662ba536618c41725313ab283c798c7d7e31d7fcd13c687c10a0ec14af559@ec2-54-225-18-166.compute-1.amazonaws.com:5432/d1at222iravvnp'
     
 class DevConfig(Config):
     DEVELOPMENT = True
